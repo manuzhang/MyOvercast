8e706b60fb42a79181ac5d7ca6985e5823186b18
manuzhang <manuzhang@users.noreply.github.com>
2021-09-13 01:47:10 +0000

    deploy: 997bf84da48c848d9e08481f027ef1e027b644a1

---
+++ custom.webpack.config.js
@@ -0,0 +1,26 @@
+const { merge } = require('webpack-merge');
+const generated = require('./scalajs.webpack.config');
+
+const local = {
+    devServer: {
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(ttf|eot|woff|png|glb|svg)$/,
+                use: 'file-loader'
+            },
+            {
+                test: /\.(eot)$/,
+                use: 'url-loader'
+            }
+        ]
+    }
+};
+
+module.exports = merge(generated, local);
---
+++ scalajs.webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "entry": {
+    "frontend-opt": ["/home/runner/work/MyOvercast/MyOvercast/js/target/scala-2.13/scalajs-bundler/main/frontend-opt.js"]
+  },
+  "output": {
+    "path": "/home/runner/work/MyOvercast/MyOvercast/js/target/scala-2.13/scalajs-bundler/main",
+    "filename": "[name]-bundle.js"
+  },
+  "mode": "production",
+  "devtool": "source-map",
+  "module": {
+    "rules": [{
+      "test": new RegExp("\\.js$"),
+      "enforce": "pre",
+      "use": ["source-map-loader"]
+    }]
+  }
+}
